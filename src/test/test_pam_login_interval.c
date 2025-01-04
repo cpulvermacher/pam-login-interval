8bb1c445461794fc442fa44bc681de2f55e5c1f6
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2025-01-04 10:00:36 +0900

    actually test pam_sm_acct_mgmt()

--- src/test/test_pam_login_interval.c
+++ src/test/test_pam_login_interval.c
@@ -22,23 +22,12 @@ static int test_conv(int num_msg, const struct pam_message **msg,
 
 struct pam_conv conv = {test_conv, NULL};
 
-int main(void)
+int test_login(int argc, const char **argv, const char *current_user)
 {
-    struct passwd *pw;
     pam_handle_t *pamh = NULL;
-    int status;
     int flags = 0;
-    int argc = 1;
-    const char *argv[] = {"min_interval=100000"};
 
-    pw = getpwuid(getuid()); // Get the current user
-    if (pw == NULL)
-    {
-        perror("getpwuid");
-        return 1;
-    }
-
-    status = pam_start("test", pw->pw_name, &conv, &pamh);
+    int status = pam_start("test", current_user, &conv, &pamh);
     if (status != PAM_SUCCESS)
     {
         fprintf(stderr, "Error in pam_start: %s\n", pam_strerror(pamh, status));
@@ -54,5 +43,69 @@ int main(void)
     {
         printf("Authentication denied: %s. Message: %s\n", pam_strerror(pamh, status), last_pam_message);
     }
+    pam_end(pamh, status);
+    return status;
+}
+
+int main(void)
+{
+    struct passwd *pw = getpwuid(getuid()); // Get the current user
+    if (pw == NULL)
+    {
+        perror("getpwuid");
+        return 1;
+    }
+
+    // log in as the current user, with zero interval
+    const char *argv1[] = {"min_interval=0"};
+    if (test_login(1, argv1, pw->pw_name) != PAM_SUCCESS)
+    {
+        printf("%s:%d: Expected PAM_SUCCESS\n", __FILE__, __LINE__);
+        return 1;
+    }
+
+    // log in as the current user, but require last login before 100 years
+    const char *argv2[] = {"min_interval=36500d"};
+    if (test_login(1, argv2, pw->pw_name) != PAM_AUTH_ERR)
+    {
+        printf("%s:%d: Expected PAM_AUTH_ERR\n", __FILE__, __LINE__);
+        return 1;
+    }
+
+    char target_user_arg[100];
+    snprintf(target_user_arg, sizeof(target_user_arg), "target_user=%s", pw->pw_name);
+
+    // log in as the current user, with zero interval (target_user set)
+    const char *argv3[] = {target_user_arg, "min_interval=0"};
+    if (test_login(2, argv3, pw->pw_name) != PAM_SUCCESS)
+    {
+        printf("%s:%d: Expected PAM_SUCCESS\n", __FILE__, __LINE__);
+        return 1;
+    }
+
+    // log in as the current user, but require last login before 100 years (target_user set)
+    const char *argv4[] = {target_user_arg, "min_interval=36500d"};
+    if (test_login(2, argv4, pw->pw_name) != PAM_AUTH_ERR)
+    {
+        printf("%s:%d: Expected PAM_AUTH_ERR\n", __FILE__, __LINE__);
+        return 1;
+    }
+
+    // log in as other user, with zero interval (target_user set)
+    const char *argv5[] = {"target_user=A", "min_interval=0"};
+    if (test_login(2, argv5, "B") != PAM_SUCCESS)
+    {
+        printf("%s:%d: Expected PAM_SUCCESS\n", __FILE__, __LINE__);
+        return 1;
+    }
+
+    // log in as other user, but require last login before 100 years (target_user set)
+    const char *argv6[] = {"target_user=A", "min_interval=36500d"};
+    if (test_login(2, argv6, "B") != PAM_SUCCESS)
+    {
+        printf("%s:%d: Expected PAM_SUCCESS\n", __FILE__, __LINE__);
+        return 1;
+    }
+
     return 0;
 }
